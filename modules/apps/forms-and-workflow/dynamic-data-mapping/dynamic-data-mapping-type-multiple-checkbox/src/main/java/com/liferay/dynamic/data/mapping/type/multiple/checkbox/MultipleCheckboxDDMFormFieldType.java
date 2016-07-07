da7c90a124c17e942d78fea46dd8abb81184108a
Rafael Praxedes <rafael.praxedes@liferay.com>
2016-07-07 15:19:01 -0300

    LPS-65804 Set the label property to the multipleCheckbox fieldType

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-multiple-checkbox/src/main/java/com/liferay/dynamic/data/mapping/type/multiple/checkbox/MultipleCheckboxDDMFormFieldType.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-multiple-checkbox/src/main/java/com/liferay/dynamic/data/mapping/type/multiple/checkbox/MultipleCheckboxDDMFormFieldType.java
@@ -30,6 +30,7 @@ import org.osgi.service.component.annotations.Component;
 		"ddm.form.field.type.icon=check-circle",
 		"ddm.form.field.type.js.class.name=Liferay.DDM.Field.MultipleCheckbox",
 		"ddm.form.field.type.js.module=liferay-ddm-form-field-multiple-checkbox",
+		"ddm.form.field.type.label=multiple-checkbox-field-type-label",
 		"ddm.form.field.type.name=multiple-checkbox"
 	},
 	service = DDMFormFieldType.class
