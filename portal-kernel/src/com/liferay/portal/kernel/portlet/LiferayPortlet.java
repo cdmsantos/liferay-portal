177fca71146e5fd2c2aca1ebf598122826cd180a
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-05-11 09:20:47 -0300

    LPS-58771 Log

--- portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
@@ -193,12 +193,10 @@ public class LiferayPortlet extends GenericPortlet {
 				return true;
 			}
 			catch (Exception e) {
-				if (_log.isInfoEnabled()) {
-					_log.info(
-						String.format(
-							"actionName = %s, namespace =", actionName,
-							actionResponse.getNamespace()));
-				}
+				_log.error(
+					String.format(
+						"actionName = %s, namespace =", actionName,
+						actionResponse.getNamespace()));
 
 				throw new PortletException(e);
 			}
